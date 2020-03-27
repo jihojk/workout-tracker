66535167dfc79cd6bd79d67cd38333b026c2f136
Jiho Kim <jihojk@uci.edu>
2020-03-26 18:45:49 -0700

    mongodb heroku: L

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require("express");
 const mongoose = require("mongoose");
 const PORT = process.env.PORT || 3000;
 
-mongoose.connect("mongodb://localhost/workout", {
+mongodb+srv://jihojk:<usuckman11>@cluster0-dux9w.azure.mongodb.net/test?retryWrites=true&w=majority
+
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
