f4215941f03817041bbeacf61a5c6a7b618157ac
Jiho Kim <jihojk@uci.edu>
2020-03-28 11:28:02 -0700

    bugfix

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,10 +1,13 @@
-var path = require("path");
-var db = require("../models")
+const path = require("path");
+const db = require("../models")
 
 function apiRoutes(app) {
 
     app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public", "index.html"));
+        res.sendFile(path.join(__dirname, "../public", "index.html"))
+        db.Workout.find({}).sort({day: 1}).limit(1).then(dbLatest => {
+          res.json(dbLatest);
+        });
       });
       
       //exercise route
@@ -18,39 +21,40 @@ function apiRoutes(app) {
 
       // get all existing workouts
       app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(data => {
-          res.json(data);
+        db.Workout.find({}).then(dbWorkout => {
+          res.json(dbWorkout);
         });
       });
       
       //create a new workout
       app.post("/api/workouts", (req, res) => {
-        console.log(req.body);
-        db.Workout.create({}).then(result => {
-     
-          res.json(result);
-        });
+        db.Workout.create(req.body).then(function (data){
+          db.Workout.findOneAndUpdate({ _id: data._id},{$push:{exercises:req.body}}, function(error, success){
+
+            res.json(result)
+          })
+        })
       });
       
 
-      app.put("/api/workouts/:id", (req, res) => {
-        let id = req.params.id;
-        let { body } = req;
-        let increaseDuration = body.duration;
-        db.Workout.findByIdAndUpdate(
-          {
-            _id: id
-          },
-          {
-            $push: {
-              exercises: body
-            },
-            $inc: {
-              totalDuration: increaseDuration
-            }
-          }
-        ).then(result => res.json(result));
-      });
+      // app.put("/api/workouts/:id", (req, res) => {
+      //   let id = req.params.id;
+      //   let { body } = req;
+      //   let increaseDuration = body.duration;
+      //   db.Workout.findByIdAndUpdate(
+      //     {
+      //       _id: id
+      //     },
+      //     {
+      //       $push: {
+      //         exercises: body
+      //       },
+      //       $inc: {
+      //         totalDuration: increaseDuration
+      //       }
+      //     }
+      //   ).then(result => res.json(result));
+      // });
 
       app.get("/api/workouts/range", function (req, res){
         db.Workout.find({})
--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 //var morgan = require('morgan');
 const app = express();
 
+
 const PORT = process.env.PORT || 3000;
 
 // mongodb+srv://jihojk:<usuckman11>@cluster0-dux9w.azure.mongodb.net/test?retryWrites=true&w=majority
@@ -17,8 +18,8 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-var apiRoutes = require("./routes/api-routes");
+// routes
+var apiRoutes = require("./routes/api-routes.js");
 apiRoutes(app);
 
 app.listen(PORT, () => {
