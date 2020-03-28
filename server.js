f130e5891e0e22217c0dc4a30afc297ed5b8a5bc
Jiho Kim <jihojk@uci.edu>
2020-03-28 11:09:52 -0700

    debugg1

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const mongoose = require("mongoose");
-var morgan = require('morgan');
+//var morgan = require('morgan');
 const app = express();
 
 const PORT = process.env.PORT || 3000;
