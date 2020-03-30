b95e3420a1b394a558d5ff5d9809c12bda0f801b
Jiho Kim <jihojk@uci.edu>
2020-03-30 15:24:20 -0700

    bug fix

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,70 +1,46 @@
-const path = require("path");
-const db = require("../models")
+var path = require("path");
+var db = require("../models")
 
 function apiRoutes(app) {
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+        db.Workout.find({}).sort({ day: 1 }).limit(1)
+            .then(dbLatest => {
+                res.json(dbLatest);
+            })
+    })
 
-    app.get("/", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public", "index.html"))
-        db.Workout.find({}).sort({day: 1}).limit(1).then(dbLatest => {
-          res.json(dbLatest);
-        });
-      });
-      
-      //exercise route
-      app.get("/exercise", (req, res) => {
-        res.sendFile(path.join(__dirname, "../public", "exercise.html"));
-      });
-      //stats route
-      app.get("/stats", function (req, res) {
+    app.get("/exercise", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/exercise.html"))
+    })
+
+    app.get("/stats", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/stats.html"))
     })
 
-      // get all existing workouts
-      app.get("/api/workouts", (req, res) => {
-        db.Workout.find({}).then(dbWorkout => {
-          res.json(dbWorkout);
-        });
-      });
-      
-      //create a new workout
-      app.post("/api/workouts", (req, res) => {
-        db.Workout.create(req.body).then(function (data){
-          db.Workout.findOneAndUpdate({ _id: data._id},{$push:{exercises:req.body}}, function(error, success){
-
-            res.json(result)
-          })
-        })
-      });
-      
-
-      // app.put("/api/workouts/:id", (req, res) => {
-      //   let id = req.params.id;
-      //   let { body } = req;
-      //   let increaseDuration = body.duration;
-      //   db.Workout.findByIdAndUpdate(
-      //     {
-      //       _id: id
-      //     },
-      //     {
-      //       $push: {
-      //         exercises: body
-      //       },
-      //       $inc: {
-      //         totalDuration: increaseDuration
-      //       }
-      //     }
-      //   ).then(result => res.json(result));
-      // });
-
-      app.get("/api/workouts/range", function (req, res){
+    app.get("/api/workouts", function (req, res) {
         db.Workout.find({})
         .then (dbWorkout => {
             res.json(dbWorkout);
         })
     })
 
+    app.post("/api/workouts", function (req, res) {
+        db.Workout.create(req.body).then(function (data) {
+            db.Workout.findOneAndUpdate({ _id: data._id }, { $push: { exercises: req.body } }, function (error, success) {
+                res.json(success)
+            })
+        })
+    })
 
+    app.get("/api/workouts/range", function (req, res){
+        db.Workout.find({})
+        .then (dbWorkout => {
+            res.json(dbWorkout);
+        })
+    })
 
+    
 }
 
 module.exports = apiRoutes;
